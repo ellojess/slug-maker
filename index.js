fec930745bbcb6a48b03e826f034ad204b812df6
Jess <jtrinh619@gmail.com>
2021-05-06 13:32:21 -0700

    initial commit, project setup

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('hello world!');
+
+module.exports = {
+    sayHello: () => {
+        console.log('Say Hello!');
+    }
+}
---
+++ tester/index.js
@@ -0,0 +1,3 @@
+const {sayHello} = require('slug-maker');
+
+sayHello();
